77550c5e149dd4f72a395ce9d2cf1b260e8f15cb
Raymond Dury <rdury@outlook.com>
2017-03-08 16:10:14 -0500

    Add project files.

---
+++ WSRD1/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WSRD1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
